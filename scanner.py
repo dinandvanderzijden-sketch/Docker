59cfd2ff8a4e326320ddf7b23dd7e097754748a0
Dinand van der Zijden <dinandvanderzijden@gmail.com>
2026-06-19 09:45:41 +0200

    Testing

--- scanner.py
+++ scanner.py
@@ -130,7 +130,7 @@ def packet_callback(packet):
         check_mac_baseline(src_mac, src_ip)
 
 if __name__ == "__main__":
-    print("[+] Passive Network Sniffer v1.1 wordt opgestart...")
+    print("[+] Passive Network Sniffer v1.2 wordt opgestart...")
     
     init_database()
     
